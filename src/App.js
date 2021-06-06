e96be9393c181b218243833dcfdcd25815d65aa4
kamendra singh gour <kamendrasau@gmail.com>
2021-06-06 15:03:30 +0530

    First React app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./Mycomponents/Header";
+import {Todos} from "./Mycomponents/Todos";
+import {Footer} from "./Mycomponents/Footer";
+
+
+function App() {
+  return (
+    <>
+     <Header title="Welcome"/>
+     <Todos/>
+     <Footer/>
+    </>
+
+  );
+}
+
+export default App;
